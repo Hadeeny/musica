76bcf497dfb717f48f83106390dd121526b4ba24
hadeeny <oluwasegunadeniyi064@gmail.com>
2022-11-06 07:37:48 +0100

    top charts

--- src/components/TopCharts.jsx
+++ src/components/TopCharts.jsx
@@ -1,33 +1,15 @@
 import { useState, useEffect, useRef } from "react";
 import { motion } from "framer-motion";
 import { FcLikePlaceholder } from "react-icons/fc";
-import golden from "../assets/golden.png";
-import reggae from "../assets/reggae.png";
-import tomorrow from "../assets/tomorrow.png";
-
+import {Link} from 'react-router-dom'
+import {useSelector} from 'react-redux'
 const TopCharts = () => {
   const [width, setWidth] = useState(0);
-  const [topChart, setTopChart] = useState([
-    {
-      album: "Golden age of 80s",
-      title: "Sean Swadder",
-      duration: "2:21:35",
-      cover: golden,
-    },
-    {
-      album: 'Raggae "n" blues',
-      title: "DJ YK mule",
-      duration: "1:02:42",
-      cover: reggae,
-    },
-    {
-      album: "Tomorrows tunes",
-      title: "Obi Datti",
-      duration: "2:01:25",
-      cover: tomorrow,
-    },
-  ]);
+ 
   const carousel = useRef();
+
+  const playlist = useSelector(state => state.playlist)
+  const {myPlaylist }= playlist
 
   useEffect(() => {
     // console.log(carousel.current);
@@ -42,8 +24,10 @@ const TopCharts = () => {
           dragConstraints={{ right: 0, left: -width }}
           className="flex gap-8 lg:gap-4 lg:flex-col"
         >
-          {topChart.map((chart, index) => {
+          {myPlaylist.slice(0, 3).map((chart, index) => {
             return ( 
+              <Link to={`album/${chart.id}`}>
+              
               <div
                 key={index}
                 className="flex justify-between min-h-[6em] min-w-[22em] p-2 bg-darkGray rounded-xl lg:h-[4em]"
@@ -56,13 +40,14 @@ const TopCharts = () => {
                     className="lg:w-[4em] lg:h-[4em]"
                   />
                   <div className='p-2'>
-                    <h3 className="">{chart.album}</h3>
-                    <p className="text-sm ">{chart.title}</p>
-                    <div>{chart.duration}</div>
+                    <h3 className="">{chart.title}</h3>
+                    <p className="text-sm ">{chart.info.slice(0, 30)}</p>
+                    <div>{chart.title}</div>
                   </div>
                 </div>
                 <FcLikePlaceholder />
               </div>
+              </Link>
             );
           })}
         </motion.div>
